be6d0bc739535e7b60827391b0d4a485a794aa7f
Edvin Tewolde Berhane <edvin_tewoldeberhane@college.harvard.edu>
2021-03-20 14:41:53 +0100

    Initial Commit

---
+++ ClassPlanner/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  ClassPlanner
+//
+//  Created by Edvin Berhan on 2021-03-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
