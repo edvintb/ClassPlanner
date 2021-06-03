54935ff42fe3cb1830ed513bb41e09340732b094
Edvin Tewolde Berhane <edvin_tewoldeberhane@college.harvard.edu>
2021-06-03 16:47:47 +0200

    where I wanted to be

---
+++ ClassPlanner/Views/PanelViews/PanelConcentrations.swift
@@ -0,0 +1,28 @@
+//
+//  PanelConcentrations.swift
+//  ClassPlanner
+//
+//  Created by Edvin Berhan on 2021-06-03.
+//
+
+import SwiftUI
+
+struct PanelConcentrations: View {
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            Text("Concentrations")
+            Spacer()
+        }
+    }
+}
+
+//struct PanelConcentrations_Previews: PreviewProvider {
+//    @Environment(\.managedObjectContext) var context
+//    let conc = Concentration(context: context)
+//
+//    static var previews: some View {
+//        PanelConcentrations()
+//    }
+//}
--- ClassPlanner/Views/PanelViews/PanelView.swift
+++ ClassPlanner/Views/PanelViews/PanelView.swift
@@ -28,7 +28,73 @@ struct PanelView: View {
                         .onTapGesture { shared.setPanelSelection(to: option) }
                     Spacer()
                 }
-                
+            }
+            Spacer().frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 5, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            Divider()
+            
+
+            getPanelContent(shared.currentPanelSelection)
+        }
+
+    }
+    
+    
+    @ViewBuilder
+    func getPanelContent(_ selection: PanelOption) -> some View {
+        switch selection {
+        case .editor:
+            getEditor(shared.currentEditSelection)
+        case .courses:
+            PanelCoursesView(courseStore: courseStore, scheduleStore: scheduleStore)
+        case .concentrations:
+            PanelConcentrations()
+        case .schedules:
+            PanelSchedules(store: scheduleStore)
+        case .otherPeople:
+            Text("Other People")
+        }
+    }
+    
+    @ViewBuilder
+    func getEditor(_ selection: EditOption) -> some View {
+        switch selection {
+        case .course(let course):
+            CourseEditorView(course: course, scheduleStore: scheduleStore, courseSuggestionVM: courseSuggestionVM, context: context)
+        case .category(let category):
+            CategoryEditorView(category: category, courseStore: CourseStore(context: context, shared: shared))
+        case .concentration(let concentration):
+            Text("Concentration: \(concentration.name)")
+        case .schedule(let schedule):
+            ScheduleEditorView(schedule: schedule, scheduleStore: scheduleStore)
+                        .alert(isPresented: $scheduleStore.doubleNameAlert) {
+                            Alert(title: Text("Naming Conflict"),
+                                  message: Text("A schedule with that name already exists.\nPlease pick another name."),
+                                  dismissButton: .default(Text("OK"))
+                        )}
+//                .onDisappear { scheduleStore.setName(schedule.name, for: schedule) }
+        case .none:
+            VStack {
+                Spacer()
+                Text("No Selection")
+                    .font(.system(size: 15))
+                    .opacity(0.3)
+                Spacer()
+            }
+            
+        }
+        
+        
+    }
+}
+
+
+//            TabView(selection: $viewModel.currentPanelSelection,
+//                    content:  {
+//                        Text("Tab Content 1").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 1")/*@END_MENU_TOKEN@*/ }.tag(1)
+//                        Text("Tab Content 2").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 2")/*@END_MENU_TOKEN@*/ }.tag(2)
+//                    })
+
+
 //                Group {
 //                    Spacer()
 //                    Text("Editor")
@@ -54,70 +120,6 @@ struct PanelView: View {
 //                        .onTapGesture { panel.setPanelSelection(to: .otherPeople) }
 //                    Spacer()
 //                }
-            }
-            Spacer().frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 5, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-            Divider()
-            
-
-            getPanelContent(shared.currentPanelSelection)
-        }
-        
-        //            TabView(selection: $viewModel.currentPanelSelection,
-        //                    content:  {
-        //                        Text("Tab Content 1").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 1")/*@END_MENU_TOKEN@*/ }.tag(1)
-        //                        Text("Tab Content 2").tabItem { /*@START_MENU_TOKEN@*/Text("Tab Label 2")/*@END_MENU_TOKEN@*/ }.tag(2)
-        //                    })
-    }
-    
-    
-    @ViewBuilder
-    func getPanelContent(_ selection: PanelOption) -> some View {
-        switch selection {
-        case .courses:
-            PanelCoursesView(courseStore: courseStore, scheduleStore: scheduleStore)
-        case .concentrations:
-            Text("Concentrations")
-        case .editor:
-            getEditor(shared.currentEditSelection)
-        case .schedules:
-            PanelSchedules(store: scheduleStore)
-        case .otherPeople:
-            Text("Other People")
-        }
-    }
-    
-    @ViewBuilder
-    func getEditor(_ selection: EditOption) -> some View {
-        switch selection {
-        case .course(let course):
-            CourseEditorView(course: course, scheduleStore: scheduleStore, courseSuggestionVM: courseSuggestionVM, context: context)
-
-        case .category(let category):
-            CategoryEditorView(category: category, courseStore: CourseStore(context: context, shared: shared))
-        case .concentration(let concentration):
-            Text("Concentration: \(concentration.name)")
-        case .schedule(let schedule):
-            ScheduleEditorView(schedule: schedule, scheduleStore: scheduleStore)
-                        .alert(isPresented: $scheduleStore.doubleNameAlert) {
-                            Alert(title: Text("Naming Conflict"),
-                                  message: Text("A schedule with the name already exists.\nPlease pick another name."),
-                                  dismissButton: .default(Text("OK"))
-                        )}
-//                .onDisappear { scheduleStore.setName(schedule.name, for: schedule) }
-        case .none:
-            VStack {
-                Spacer()
-                Text("No Selection")
-                    .font(.system(size: 15))
-                    .opacity(0.3)
-                Spacer()
-            }
-            
-        }
-        
-        
-    }
-}
 
 //struct PanelView_Previews: PreviewProvider {
 //    static var previews: some View {
